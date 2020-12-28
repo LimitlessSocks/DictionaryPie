1ce1588b3a1bfb9adc4c70d69c82d543e0a51e5c
ConorOBrien <csobrien@email.wm.edu>
2020-12-27 20:21:55 -0500

    initial commit

---
+++ needed.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+const nexusDb = JSON.parse(fs.readFileSync("db.json"));
+
+fs.readdir("./large", (err, files) => {
+    let keys = Object.keys(nexusDb);
+    let fileKeys = files.map(fileName => parseInt(fileName, 10).toString());
+    
+    let diffKeys = keys.filter(key => fileKeys.indexOf(key) === -1);
+    
+    fs.writeFile("ids.json", JSON.stringify(diffKeys),
+        () => console.log("done, wrote " + diffKeys.length + " id(s) to ids.json")
+    );
+});
+
+/*
+//CODE TO GET NEXUS IDS
+(function(){
+    let json = JSON.stringify(Engine.database.cards);
+    EXT.download(json, "db.json", "text/json")
+})();
+*/
