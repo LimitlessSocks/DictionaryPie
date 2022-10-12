87ec73b1b8d3eba3ae095116db6d2c13dfd0bffc
LimitlessSocks <socktheringleader@gmail.com>
2022-10-12 12:13:07 -0400

    update

--- needed.js
+++ needed.js
@@ -1,13 +1,18 @@
 const fs = require("fs");
 
+console.log("Reading file...");
 const nexusDb = JSON.parse(fs.readFileSync("db.json"));
 
 fs.readdir("./small", (err, files) => {
+    console.log("Getting keys...");
     let keys = Object.keys(nexusDb);
+    console.log("Mapping names...");
     let fileKeys = files.map(fileName => parseInt(fileName, 10).toString());
     
+    console.log("Getting differences...");
     let diffKeys = keys.filter(key => fileKeys.indexOf(key) === -1);
     
+    console.log("Writing file...");
     fs.writeFile("ids.json", JSON.stringify(diffKeys),
         () => console.log("done, wrote " + diffKeys.length + " id(s) to ids.json")
     );
@@ -15,8 +20,14 @@ fs.readdir("./small", (err, files) => {
 
 /*
 //CODE TO GET NEXUS IDS
-(function(){
-    let json = JSON.stringify(Engine.database.cards);
-    EXT.download(json, "db.json", "text/json")
-})();
+function downloadObjectAsJson(exportObj, exportName){
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href",     dataStr);
+    downloadAnchorNode.setAttribute("download", exportName + ".json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+  }
+downloadObjectAsJson(Engine.database.cards, "db")
 */
